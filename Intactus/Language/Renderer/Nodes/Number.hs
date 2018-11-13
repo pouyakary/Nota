607873032c6da0fe3c129ad9fbb6bb73beb0109d
pmkary <pouya@kary.us>
2018-11-14 00:37:58 +0330

    no more 5.0 in number representation

--- Intactus/Language/Renderer/Nodes/Number.hs
+++ Intactus/Language/Renderer/Nodes/Number.hs
@@ -6,12 +6,15 @@ module Language.Renderer.Nodes.Number ( rendereASTNumber ) where
 import Language.FrontEnd.Types
 import Infrastructure.Text.Layout
 import Data.Scientific
+import Text.Regex
 
 -- ─── RENDER ─────────────────────────────────────────────────────────────────────
 
 rendereASTNumber :: Scientific -> SpacedBox
 rendereASTNumber x =
-    spacedBox value where
+    spacedBox noZeroDecimalValue where
+        noZeroDecimalValue =
+            subRegex ( mkRegex ".0$" ) value ""
         value =
             if length stringedNumber > 10
                 then formatScientific Generic ( Just 10 ) x
