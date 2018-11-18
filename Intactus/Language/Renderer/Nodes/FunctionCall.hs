46bf188a51a10ff4436829f614c28cff3db9d9b0
pmkary <pouya@kary.us>
2018-11-19 00:15:13 +0330

    awesome function rendering

--- Intactus/Language/Renderer/Nodes/FunctionCall.hs
+++ Intactus/Language/Renderer/Nodes/FunctionCall.hs
@@ -41,30 +41,35 @@ renderSimpleFunction name args render = result where
         verticalConcat [ boxedName, parenthesisedArgs ]
 
 functionArgsConcat :: [ SpacedBox ] -> SpacedBox
-functionArgsConcat boxes = SpacedBox { boxLines = resultLines
-                                     , width    = resultWidth
-                                     , height   = resultHeight
-                                     , baseLine = resultBaseLine
-                                     }
-    where
-        resultHeight =
-            maximum [ length ( boxLines x ) | x <- boxes ]
-        centeredBoxlines =
-            [ centerText ( width x ) resultHeight x | x <- boxes ]
-        resultWidth =
-            sum [ 2 + width x | x <- boxes ] - 2
-        resultLines =
-            [ intercalate ( connectorOfLine lineNumber ) [ ( boxLines x ) !! lineNumber
-                | x <- centeredBoxlines ]
-                    | lineNumber <- [ 0.. ( resultHeight - 1 ) ] ]
-        resultCenter =
-            resultHeight `div` 2
-        connectorOfLine x =
-            if x == resultCenter then ", " else "  "
-        resultBaseLine =
-            if odd resultHeight
-                then resultHeight `div` 2
-                else resultHeight `div` 2
+functionArgsConcat inputBoxes = result where
+    boxes =
+        [ baselineCentered x | x <- inputBoxes ]
+    resultHeight =
+        maximum [ length ( boxLines x ) | x <- boxes ]
+    centeredBoxlines =
+        [ centerText ( width x ) resultHeight x | x <- boxes ]
+    resultWidth =
+        sum [ 2 + width x | x <- boxes ] - 2
+    resultLines =
+        [ intercalate ( connectorOfLine lineNumber ) [ ( boxLines x ) !! lineNumber
+            | x <- centeredBoxlines ]
+                | lineNumber <- [ 0.. ( resultHeight - 1 ) ] ]
+    resultCenter =
+        resultHeight `div` 2
+    connectorOfLine x =
+        if x == resultCenter then ", " else "  "
+    resultBaseLine =
+        if odd resultHeight
+            then resultHeight `div` 2
+            else resultHeight `div` 2
+    resultWithoutTrim =
+        SpacedBox { boxLines = resultLines
+                  , width    = resultWidth
+                  , height   = resultHeight
+                  , baseLine = resultBaseLine
+                  }
+    result =
+        trimWhiteSpaceLines resultWithoutTrim
 
 -- ─── GENERAL BOXED TYPE FUNCTION RENDERER ───────────────────────────────────────
 
