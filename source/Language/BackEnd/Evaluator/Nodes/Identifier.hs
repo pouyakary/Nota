2cad40609a05cff797b00a7091514010f0d88840
pmkary <pouya@kary.us>
2019-05-14 03:11:58 +0430

    added the Karion easter

--- source/Language/BackEnd/Evaluator/Nodes/Identifier.hs
+++ source/Language/BackEnd/Evaluator/Nodes/Identifier.hs
@@ -5,16 +5,52 @@ module Language.BackEnd.Evaluator.Nodes.Identifier ( evalIdentifier ) where
 
 import           Data.Map ( Map )
 import qualified Data.Map as Map
+import           Data.Time
 import           Language.BackEnd.Evaluator.Types
 import           Language.FrontEnd.AST
 import           Model
+import           System.IO.Unsafe
 
 -- ─── EVAL IDENTIFIER ────────────────────────────────────────────────────────────
 
 evalIdentifier :: LeafEvalSignature
 evalIdentifier ( ASTIdentifier name ) scopePrototype =
+    case lookupInConstants name of
+        Just x  -> Right x
+        Nothing -> lookupInScopePrototype name scopePrototype
+
+-- ─── SCOPE PROTOTYPE ────────────────────────────────────────────────────────────
+
+lookupInScopePrototype :: String -> ScopePrototype -> EvalResult
+lookupInScopePrototype name scopePrototype =
     case Map.lookup name scopePrototype of
         Just x  -> Right x
         Nothing -> Left $ "Variable \"" ++ name ++ "\" does not exists."
+
+-- ─── CONSTANTS ──────────────────────────────────────────────────────────────────
+
+lookupInConstants :: String -> Maybe P50
+lookupInConstants name =
+    case name of
+        "π"      -> Just pi :: Maybe P50
+        "Karion" -> Just $ unsafePerformIO computeTheKarionTime
+        _        -> Nothing
+
+-- Nota's Easter Egg :D
+computeTheKarionTime :: IO P50
+computeTheKarionTime = do
+    birthday <- pure $ pouyaKaryBirthday
+    now      <- today
+    days     <- pure $ toP50 $ diffDays now birthday
+    return days
+    where
+        pouyaKaryBirthday =
+            case fromGregorianValid 1996 1 8 of
+                Just day -> day
+        today = do
+            t <- getCurrentTime
+            return $ utctDay t
+        toP50 x =
+            read ( show x ) :: P50
 
 -- ────────────────────────────────────────────────────────────────────────────────
